1f5cc00d8e526acb601d857af08dece3ad442012
Mary Waithera <njihiamary11@gmail.com>
2020-05-18 16:01:25 +0300

    create models and test cases to test models' behavior

--- news/models.py
+++ news/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['-first_name']
+
+    def save_editor(self):
+        
+        """
+        class method saves objects in the database
+        """
+        self.save()
+
+    def delete_editor(self):
+
+        """
+        class method deletes saved editors
+        """
+        self.delete()
+    
+    @classmethod
+    def display_editors(cls):
+
+        """
+        methods returns all saved editors
+        """
+        editors= Editor.objects.all()
+        return editors
+
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tag(self):
+
+        """
+        class method saves objects in the database
+        """
+        self.save()
+
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    tags = models.ForeignKey(tags, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    
+    
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,86 @@
 from django.test import TestCase
+from .models import Editor,tags,Article
+import datetime as dt
 
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    #set up function to run before each test unit
+    def setUp(self):
+
+        """
+        method runs before each test
+        """
+        self.mary = Editor(first_name='Mary',last_name='Njihia',email='njihiamary1@gmail.com')
+    
+    def test_instance(self):
+
+        """
+        method tests if objects are initialized properly
+        """
+        self.assertTrue(isinstance(self.mary,Editor))
+    
+    def test_save_method(self):
+
+        """
+        methods tests class save functionality
+        """
+        self.mary.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+    def test_delete_method(self):
+
+        """
+        method tests class delete functionality
+        """
+        self.mary.save_editor()
+        self.mary.delete_editor()
+        editor = Editor.objects.all()
+        self.assertTrue(len(editor)==0)
+
+    def test_display_all_editors(self):
+
+        """
+        method tests class display all editors functionality
+        """
+        self.mary.save_editor()
+        self.assertTrue(Editor.display_editors(),Editor.objects.all())
+    
+class TagsTestClass(TestCase):
+
+        def setUp(self):
+
+            """
+            method runs before each test
+            """
+            tags.objects.create(name="testing")
+
+
+class ArticleTestClass(TestCase):
+    
+    def setUp(self):
+        
+        """
+        method runs before each test
+        """
+        self.mary= Editor(first_name='Mary',last_name='Njihia',email='njihiamary1@gmail.com')
+        self.mary.save_editor()
+
+        self.tags = tags(name="testing")
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor=self.mary,tags=self.tags)
+
+        self.new_article.save()
+    
+    
+
+
+        
+    
+    
+
+        
+
+    
