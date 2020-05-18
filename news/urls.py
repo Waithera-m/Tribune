9d13efdfe40f00aa915eb11bbe061a552cc94abd
Mary Waithera <njihiamary11@gmail.com>
2020-05-18 17:36:12 +0300

    Alter view functions to facilitate the display of news articles

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.news_of_day,name='newsToday'),
+    path('',views.news_today,name='newsToday'),
     url(r'archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name='pastNews'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,redirect
 
-# Create your views here.
 from django.http import HttpResponse,Http404
 
 import datetime as dt
+
+from .models import Article
+
+# Create your views here.
+
+
 
 def welcome(request):
     return render(request,'welcome.html')
@@ -18,11 +23,7 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def news_of_day(request):
 
-    date = dt.date.today()
-    
-    return render(request,'all-news/today-news.html',{"date":date,})
 
 def past_days_news(request,past_date):
 
@@ -32,10 +33,17 @@ def past_days_news(request,past_date):
     
     except ValueError:
         raise Http404()
-        assert False
+        
 
     if date==dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
+
+    news = Article.todays_news(date)
 
     
-    return render(request,'all-news/past-news.html',{"date":date})
+    return render(request,'all-news/past-news.html',{"date":date,"news":news})
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request,'all-news/today-news.html',{"date":date,"news":news})
